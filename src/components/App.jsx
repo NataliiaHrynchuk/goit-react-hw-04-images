12c48799a9fe2ad0c257d3f9cabd6b2d50a49895
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-09-29 19:08:39 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,106 +1,99 @@
-import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ImageGallery } from './ImageGallery/ImageGallery.js';
 import { Button } from './Button/Button';
 import { Loader } from './Loader/Loader';
-import {Modal} from './Modal/Modal';
+import { Modal } from './Modal/Modal';
 import SearchForm from './Searchbar/SearchForm';
 import * as SC from './Searchbar/Searchbar.style';
 import * as API from 'services/api';
 
-export default class App extends Component {
-  state = {
-    imagesName: '',
-    page: 1,
-    images: [],
-    totalPage: '',
-    status: 'idle',
-    error: null,
-    showModal: false,
-    selectedImage: null,
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
+
+export default function App() {
+  const [imagesName, setImagesName] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [totalPage, setTotalPage] = useState('');
+  const [status, setStatus] = useState(Status.IDLE);
+  const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleFormSubmit = query => {
+    setImagesName(query);
+    setPage(1);
+    setImages([]);
+    setTotalPage('');
   }
 
-  handleFormSubmit = query => {
-    this.setState({
-      imagesName: query,
-      page: 1,
-      images: [],
-      totalPage: '',
-      
-    });
-  }
 
-  loadMore = () => {
-    this.setState(
-      prevState => ({
-        page: prevState.page + 1,
-      })
-    )
-  }
-  
-  toggleModal = async largeImg => {
-    await this.setState({ selectedImage: largeImg });
-    this.setState(({ showModal }) => ({
-      showModal: !showModal
-    }));
+  const toggleModal = async largeImg => {
+    await setSelectedImage(largeImg);
+    setShowModal(!showModal);
   };
-  
 
-  componentDidUpdate(prevProps, prevState) {
-    const {imagesName, page} = this.state;
-    if (prevState.imagesName!== this.state.imagesName || prevState.page !== this.state.page) {
-      this.setState({ status: 'pending' });
-      API.getImages(imagesName, page)
-        .then((data) => {
+  useEffect(() => {
+    if (!imagesName) {
+      return;
+    }
+    setStatus(Status.PENDING);
+    API
+      .getImages( imagesName, page )
+      .then((data) => {
         const totalPage = Math.ceil(data.totalHits / 12);
-          const receivedValues = data.hits.map(
-            ({ id,
+        const receivedValues = data.hits.map(
+          ({ id,
+            webformatURL,
+            largeImageURL,
+            tags,
+          }) => {
+            return {
+              id,
               webformatURL,
               largeImageURL,
               tags,
-            }) => {
-              return {
-                id,
-                webformatURL,
-                largeImageURL,
-                tags,
-              };
-            }
-          );
-        this.setState(prevState => ({
-          images: [...prevState.images, ...receivedValues],
-          totalPage: totalPage,
-          status: 'resolved',
-        }));
-        })
-      .catch (error => this.setState({ error, status: 'rejected' }));
-    }
-  }
-  render() {
-    const {page, status, error, images, totalPage, showModal, selectedImage} = this.state;
-    return (
-      <>
-        <SC.Searchbar >
-          <SearchForm onSubmit={this.handleFormSubmit}/>
-        </SC.Searchbar>
-        
-        {status === 'rejected' && <p>{error.message}</p>}
-        {images &&
-          <ImageGallery
-            images={images}
-            toggleModal={this.toggleModal}
-          />}
-        {status === 'pending' && <Loader />}
-        {page < totalPage && 
-          <Button
-            children='Load more'
-            onClick={this.loadMore}/>
+            };
           }
-        {showModal && <Modal
-          onClose={this.toggleModal}><img src={selectedImage} alt="" />
-        </Modal>
-          
-        }
-      </>
-    );
-  }
+        );
+        setImages(prevImages => [...prevImages, ...receivedValues]);
+        setTotalPage(totalPage);
+        setStatus(Status.RESOLVED);
+      })
+      .catch(error => {
+        setError(error);
+        setStatus(Status.REJECTED);
+      })
+  }, [imagesName, page]);
+
+
+  return (
+    <>
+      <SC.Searchbar >
+        <SearchForm onSubmit={handleFormSubmit} />
+      </SC.Searchbar>
+
+      {status === Status.REJECTED && <p>{error.message}</p>}
+      {images &&
+        <ImageGallery
+          images={images}
+          toggleModal={toggleModal}
+        />}
+      {status === Status.PENDING && <Loader />}
+      {page < totalPage &&
+        <Button
+          children='Load more'
+          onClick={() => setPage(page => page + 1)} />
+      }
+      {showModal && <Modal
+        onClose={toggleModal}><img src={selectedImage} alt="" />
+      </Modal>
+
+      }
+    </>
+  );
 };
--- src/components/Searchbar/SearchForm.js
+++ src/components/Searchbar/SearchForm.js
@@ -1,48 +1,41 @@
 import React from "react";
-
+import { useState } from "react";
 import {HiOutlineSearch} from "react-icons/hi";
 import { SearchFormBtn } from './SearchFormBtn';
 import * as SC from './Searchbar.style';
 
-      class SearchForm extends React.Component {
-        state = {
-        query:'',
-        }
+export default function SearchForm({ onSubmit }) {
+  const [query, setQuery] = useState('');
+                
+  const handleNameChange = event => {
+    setQuery(event.currentTarget.value.toLowerCase());
+  };
         
-        handleNameChange = event => {
-          this.setState({ query : event.currentTarget.value.toLowerCase( )});
-        };
-        
-        handleSubmit = event => {
-          event.preventDefault();
-          if (this.state.query === ' ' ) {   
-            alert('Enter images name');                       
-             return;                                                                                                   
-         }
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (query === ' ') {
+      alert('Enter images name');
+      return;
+    }
+    onSubmit(query);
+    setQuery(' ');
+  };
 
-          this.props.onSubmit(this.state.query);  
-          console.log(this.state.query); 
-          this.setState({query: ' ' })                                      
-        };
+  return (
+    <SC.SearchForm onSubmit={handleSubmit}>
+      <SearchFormBtn type="submit">
+        <HiOutlineSearch />
+      </SearchFormBtn>
 
-
-        render() {
-          return <SC.SearchForm onSubmit={this.handleSubmit}>
-            <SearchFormBtn type="submit">
-              <HiOutlineSearch/>
-            </SearchFormBtn>
-
-            <SC.SearchFormInput
-              type="text"
-              name="imagesName"
-              value={this.state.query}
-              onChange={this.handleNameChange}
-              autoFocus
-              autocomplete="off"
-              placeholder="Search images and photos"
-            />
-          </SC.SearchForm>
-        }
-}
-      
-export default SearchForm;
+      <SC.SearchFormInput
+        type="text"
+        name="imagesName"
+        value={query}
+        onChange={handleNameChange}
+        autoFocus
+        autocomplete="off"
+        placeholder="Search images and photos"
+      />
+    </SC.SearchForm>
+  )
+};
